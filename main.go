a8b1b71b7afb1047d757e6626897def5dbdcea57
Erik Haletski <erik.haletski@protonmail.com>
2026-02-11 18:49:43 +0100

    create rest api to serve products as json

--- main.go
+++ main.go
@@ -3,11 +3,14 @@ package main
 import (
 	"github.com/gin-gonic/gin"
 	"net/http"
+	"os"
+	"path/filepath"
 )
 
 func main() {
 	router := gin.Default()
 	router.GET("/ping", pong)
+	router.GET("/produkte/:file", serveJSONHandler)
   router.Run() // listens on 0.0.0.0:8080 by default
 }
 
@@ -15,4 +18,15 @@ func pong (c *gin.Context) {
 	c.JSON(http.StatusOK, gin.H{
 		"message": "pong",
 	})
+}
+
+func serveJSONHandler(c *gin.Context) {
+	filePathToRead := filepath.Join("content", "produkte", c.Param("file") + ".json")
+	
+	fileBytes, err := os.ReadFile(filePathToRead)
+	if err != nil {
+		panic(err)
+	}
+
+	c.Data(http.StatusOK, "application/json", fileBytes) 
 }
