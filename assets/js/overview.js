da850ab799f3d433676bc932ce9790f25516a22e
Anton <wischnewskki.anton@gmail.com>
2026-02-23 12:46:30 +0100

    add product labels

--- assets/js/overview.js
+++ assets/js/overview.js
@@ -1,10 +1,12 @@
 function showOverview() {
     var x = document.getElementById("produkt-id");
+    var y = document.getElementById("produkt-label");
     var nutrition = document.getElementById("nutritions-detail");
     const btnOverview = document.getElementById("btn-overview");
     const btnNutrition = document.getElementById("btn-nutrition");
 
     x.style.display = "flex";
+    y.style.display = "block";
     nutrition.style.display = "none"
     btnOverview.classList.add("active");
     btnNutrition.classList.remove("active");
@@ -12,11 +14,13 @@ function showOverview() {
 
 function showNutrition() {
     var x = document.getElementById("produkt-id");
+    var y = document.getElementById("produkt-label");
     var nutrition = document.getElementById("nutritions-detail");
     const btnOverview = document.getElementById("btn-overview");
     const btnNutrition = document.getElementById("btn-nutrition");
 
     x.style.display = "none";
+    y.style.display = "none";
     nutrition.style.display = "block";
     btnOverview.classList.remove("active");
     btnNutrition.classList.add("active");
